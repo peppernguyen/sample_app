dd3c96adf9811e3bd65863204ed2b6e600678d62
= <=>
2017-07-26 13:33:19 +0700

    Up to 7.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com"
+  before { @user = User.new(name: "Example User", email: "user@example.com",
                             password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
