3cf589a16f5ea469f767fd90ad753fb32fa77646
= <=>
2017-07-28 11:54:11 +0700

    Finish Pagnition - Chap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -24,4 +44,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+  # before filter
+
+  def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end  
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+  end
 end
